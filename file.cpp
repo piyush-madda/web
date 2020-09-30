97bb9d5f17336017aab51d47505a50c696e8fca2
EH-GD-MOHIT21 <70685094+EH-GD-MOHIT21@users.noreply.github.com>
2020-09-30 22:31:45 +0530

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -1,27 +1,26 @@
-#include<iostream> 
+/*
 
+    Function even_or_odd takes a number as input and determines whether it's true or false.
+    returns a string "is even" if it is even and "is odd" if it's a odd number.
+
+*/
+
+#include<iostream> 
 using namespace std; 
 
 string even_or_odd(int number){ 
-
     if(number%2 == 0){ 
-
-        return " is even";     } 
-
-    else{ 
-
-        return " is odd";  // if fails to execute means remainder is 1 that's odd. 
-
+        return " is even";
     } 
-
+    else{ 
+        return " is odd";  // if fails to execute means remainder is 1 that's odd. 
+    } 
 } 
 
 int main(){ 
-
     int num;       
-
     cin >> num;    // taking user input 
 
-    cout << "The Give number "<< num << even_or_odd(num);  //calling function and printing output 
-
-    return 0;} 
+    cout << "The Give number "<< num << even_or_odd(num);  //calling function with passing copy of value of variable num and printing output 
+    return 0;
+} 
